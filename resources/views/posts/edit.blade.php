4ea9864cab73afdc16ee0524770d03a8810faf28
resident <hpsoftbusiness@gmail.com>
2024-06-22 16:25:37 +0200

    generic layout for views

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name', 'Pomorski developer blog') }}</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    @stack('styles')
-</head>
-<body>
+@extends('layout')
+@section('content')
 <div id="app">
     <post-edit :edit="{{json_encode($edit)}}"></post-edit>
 </div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-<script src="{{ asset('js/app.js') }}"></script>
-
-@stack('scripts')
-
-</body>
-</html>
+@endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hubert developer blog</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    @stack('styles')
-</head>
-<body>
+@extends('layout')
+@section('content')
 <div id="app">
     <post-index :posts="{{json_encode($posts)}}"></post-index>
 </div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-<script src="{{ asset('js/app.js') }}"></script>
-@stack('scripts')
-</body>
-</html>
+@endsection
